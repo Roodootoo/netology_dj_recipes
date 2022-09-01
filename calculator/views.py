9e10595eddc0a704c037c6ad6bd36baae5c30294
Roodootoo <elena@pyzhov.ru>
2022-09-01 20:05:53 +0300

    Done

--- calculator/views.py
+++ calculator/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 DATA = {
     'omlet': {
@@ -19,6 +20,42 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
+
+def recipe_servings(recipe, servings):
+    recipe_new = {k: v * servings for k, v in recipe.items()}
+    return recipe_new
+
+
+def omlet_view(request):
+    servings = int(request.GET.get('servings', 1))
+    recipe = DATA['omlet']
+    recipe_serv = recipe_servings(recipe, servings)
+    context = {
+        'recipe': recipe_serv,
+    }
+    return render(request, 'calculator/index.html', context)
+
+
+def pasta_view(request):
+    servings = int(request.GET.get('servings', 1))
+    recipe = DATA['pasta']
+    recipe_serv = recipe_servings(recipe, servings)
+    context = {
+        'recipe': recipe_serv,
+    }
+    return render(request, 'calculator/index.html', context)
+
+
+def buter_view(request):
+    servings = int(request.GET.get('servings', 1))
+    recipe = DATA['buter']
+    recipe_serv = recipe_servings(recipe, servings)
+    context = {
+        'recipe': recipe_serv,
+    }
+    return render(request, 'calculator/index.html', context)
+    
+    
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
 # В качестве контекста должен быть передан словарь с рецептом:
